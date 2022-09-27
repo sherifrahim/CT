5aa801d1c226736c159bd3cfc1c9d7d59bd228c6
mukesh22584 <mks22584@gmail.com>
2022-09-27 13:07:49 +0530

    sm8350-common: Drop AutoHighBrightnessMode
    - We already have Display mode in LiveDisplay

--- DeviceSettings/src/org/lineageos/device/DeviceSettings/Utils.java
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/Utils.java
@@ -37,26 +37,6 @@ public class Utils {
     private static final String TAG = Utils.class.getSimpleName();
     private static boolean mServiceEnabled = false;
 
-    private static void startService(Context context) {
-        context.startServiceAsUser(new Intent(context, AutoHighBrightnessModeService.class),
-                UserHandle.CURRENT);
-        mServiceEnabled = true;
-    }
-
-    private static void stopService(Context context) {
-        mServiceEnabled = false;
-        context.stopServiceAsUser(new Intent(context, AutoHighBrightnessModeService.class),
-                UserHandle.CURRENT);
-    }
-
-    public static void enableService(Context context) {
-        if (DeviceSettings.isHBMAutobrightnessEnabled(context) && !mServiceEnabled) {
-            startService(context);
-        } else if (!DeviceSettings.isHBMAutobrightnessEnabled(context) && mServiceEnabled) {
-            stopService(context);
-        }
-    }
-
     /**
      * Write a string value to the specified file.
      * @param filename      The filename
